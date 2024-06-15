16a7b243d3a8f7fefe8b1dd70aa536cc935389c6
Tom Zakrajsek <tzakrajsek@gmail.com>
2024-06-14 19:23:58 -0700

    Add language sub-page

---
+++ app/languages/page.js
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+
+export default function Languages() {
+  return (
+    <main className="bg-primary">
+      <Navbar />
+      <section className='relative w-full h-screen mx-auto'>
+        <div className='page-setup'>
+          <h1 className='page-title'>Languages!</h1>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+
--- app/page.js
+++ app/page.js
@@ -10,22 +10,14 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <main className="">
-      {/* @/Private/path/to/file */}
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-        {/* <div className='bg-cover bg-no-repeat bg-center'> */}
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <div className='relative z-0'>
-          <Contact />
-        </div>
+    <main className="bg-primary">
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
       </div>
-
+      <About />
+      <Experience />
+      <Contact />
     </main>
   );
 }
