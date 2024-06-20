2fd1a7d8654059b9aa50b88c7b12d2f0505fd943
Tom Zakrajsek <tzakrajsek@gmail.com>
2024-06-19 19:14:43 -0700

    Experiment with react-chrono

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -12,12 +12,61 @@ import {
 import { motion } from "framer-motion";
 
 import "react-vertical-timeline-component/style.min.css";
+import { Chrono } from "react-chrono";
 
 import { experiences } from "../constants";
 import { AnimatedSection } from ".";
 import { textVariant } from "../utils/motion";
 import Image from "next/image";
 
+// title: "Secure Systems Group",
+// company_name: "Qualcomm Technologies, Inc.",
+// icon: snapdragon,
+// iconBg: "#112",
+// date: "Oct 2017 - Dec 2023",
+// points: [
+//   "Developing and maintaining software in C++, C, Arm Assembly, Python, and Rust.",
+//   "Participating in architecture, design, and code reviews, providing constructive feedback to the team.",
+// ],
+// detailed_points: [
+//   "Led a global team of 30+ engineers responsible for end-to-end delivery of Qualcomm's TrustZone software. The team was highly execution focused, and process optimization and robustness were key to success through scalability.",
+//   "Launched 20+ Snapdragon™ processors on Android, Windows, iOS, Embedded Linux, and Chrome OS.",
+// ],
+// languages: [
+//   "rust", "c++",
+// ],
+
+// title: "May 1940",
+// cardTitle: "Dunkirk",
+// url: "http://www.history.com",
+// cardSubtitle: "Men of the British Expeditionary Force (BEF) wade out to..",
+// cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+
+
+const ChronoItem = (experience) => {
+  // console.log(experience);
+  let chrono_item = {};
+  chrono_item.title = experience.date;  // "Title";
+  chrono_item.cardTitle = experience.title;  // "cardTitle";
+  chrono_item.url = "https://google.com";
+  chrono_item.cardSubtitle = experience.company_name;  // "cardSubTitle";
+  chrono_item.cardDetailedText = experience.points; // "cardDetailedText";  // undefined;
+  // chrono_item.media = undefined;
+  // chrono_item.date = undefined;
+  // chrono_item.timelineContent = undefined;
+  return chrono_item;
+}
+
+const ChronoIcon = (experience) => {
+  // console.log(experience);
+  let chrono_item = {};
+  chrono_item.title = experience.date;  // "Title";
+  chrono_item.cardTitle = experience.title;  // "cardTitle";
+  chrono_item.url = "https://google.com";
+  chrono_item.cardSubtitle = experience.company_name;  // "cardSubTitle";
+  chrono_item.cardDetailedText = experience.points; // "cardDetailedText";  // undefined;
+  return chrono_item;
+}
 const ExperienceCard = ({ experience }) => {
   return (
     <VerticalTimelineElement
@@ -141,6 +190,35 @@ const ExperienceCard = ({ experience }) => {
 };
 
 const Experience = () => {
+  const items = [
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle: "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+      // media: {
+      //   type: "IMAGE",
+      //   source: {
+      //     url: "http://someurl/image.jpg"
+      //   }
+      // }
+    },
+    {
+      title: "May 1941",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle: "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+      // media: {
+      //   type: "IMAGE",
+      //   source: {
+      //     url: "http://someurl/image.jpg"
+      //   }
+      // }
+    },
+  ];
+
   return (
     <>
       <motion.div variants={textVariant()}>
@@ -153,14 +231,34 @@ const Experience = () => {
       </motion.div>
 
       <div className='mt-20 flex flex-col'>
-        <VerticalTimeline>
+
+        <Chrono
+          items={experiences.map((experience, index) => (ChronoItem(experience)))}
+          // disableInteraction
+          enableLayoutSwitch={false}
+          enableQuickJump={false}
+          disableToolbar
+          toolbarPosition='BOTTOM'
+          mode='VERTICAL_ALTERNATING'
+          // mode='HORIZONTAL'
+          theme={{
+            primary: '#aaddff',
+            secondary: 'black',
+            cardBgColor: '#004466',
+            titleColor: 'white',
+            titleColorActive: 'orange',
+          }}
+        ></Chrono>
+
+        {/* <VerticalTimeline>
           {experiences.map((experience, index) => (
             <ExperienceCard
               key={`experience-${index}`}
               experience={experience}
             />
+
           ))}
-        </VerticalTimeline>
+        </VerticalTimeline> */}
       </div>
     </>
   );
