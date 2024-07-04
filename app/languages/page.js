f426657d9df036e196bb578f410fddc68082ee16
Tom Zakrajsek <tzakrajsek@gmail.com>
2024-07-03 17:36:21 -0700

    Removed dead code

--- app/languages/page.js
+++ app/languages/page.js
@@ -6,23 +6,8 @@ import { languages } from "../data/languages";
 const LanguageDescription = ({ index, title, icon, id, desc }) => (
   <div>
     <a id={id} />
-    {/* pt-20 below is to count the navbar.  If the navbar hides, remove it*/}
-
-    {/* <div className='mt-4 flex flex-wrap gap-4'>
-      <div className='w-40'>
-        <Image src={icon} alt={title} className='w-24 h-24 object-contain justify-start' />
-      </div>
-      <div className='w-96 text-2xl'>
-        <h2>{title}</h2>
-        {desc.map((paragraph, index) => (
-          <p key={index} className='pt-4'>{paragraph}</p>
-        ))}
-      </div>
-    </div> */}
-
     <div className='mt-12 text-2xl'>
       <h2 className='mb-4'>{title}</h2>
-
       {desc.map((paragraph, index) => (
         <p key={index} className='text-primary pt-4'>{paragraph}</p>
       ))}
@@ -39,13 +24,11 @@ export default function Languages() {
         <h1>Programming Languages</h1>
 
         <div className='flex flex-row mt-6 text-primary-light font-bold text-2xl '>
-          {/* <div className='flex flex-row'> */}
           <div className='space-x-8'>
             {languages.map((language, index) => (
               <a href={'#' + language.id} className="link"> {language.title} </a>
             ))}
           </div>
-
         </div>
 
         {languages.map((language, index) => (
