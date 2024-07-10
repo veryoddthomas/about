92f4fba75049425af5e0dc50fe74a15141747600
Tom Zakrajsek <tzakrajsek@gmail.com>
2024-07-10 15:32:36 -0700

    Fix IconCard spacing

--- app/components/IconCard.jsx
+++ app/components/IconCard.jsx
@@ -6,24 +6,28 @@ import { fadeIn, textVariant } from "../utils/motion";
 const IconCard = ({ index, title, icon, url }) => (
     <motion.div
         variants={fadeIn("right", "spring", index * 0.15, 1.50)}
-        className='w-[150px] h-[150px] bg-primary-dark p-[1px] rounded-[10px] shadow-card'
+        className='bg-primary-dark p-[1px] rounded-[10px] shadow-card'
     >
         <a
             href={url}
-            className='group rounded-[20px] border border-primary-light px-5 py-4  hover:border-primary-light hover:bg-secondary-dark flex justify-evenly items-center flex-col'
+            className='w-[150px] h-[150px] group rounded-[20px] border border-primary-light px-5 py-4  hover:border-primary-light hover:bg-secondary-dark flex justify-evenly items-center flex-col'
             // target="_blank"
             rel="noopener noreferrer"
         >
-            <Image
-                src={icon}
-                alt={title}
-                className='w-16 h-16 object-contain'
-            />
+            <div className='w-16 h-16'>
+                <Image
+                    src={icon}
+                    alt={title}
+                    className='w-16 h-16 object-contain'
+                />
 
-            <br />
-            <h3 className='text-primary-light text-[16px] text-center'>
-                {title}
-            </h3>
+                <br />
+            </div>
+            <div>
+                <h3 className='text-primary-light text-[16px] text-center'>
+                    {title}
+                </h3>
+            </div>
         </a>
     </motion.div>
 );
