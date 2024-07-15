8c24a102e8544d4e189bb1586ed943741e9afc0a
Tom Zakrajsek <tzakrajsek@gmail.com>
2024-07-14 17:29:04 -0700

    Add proficiency (with tooltip) to languages

--- app/languages/page.jsx
+++ app/languages/page.jsx
@@ -1,11 +1,19 @@
-import { languages } from "../data/languages";
+import { languages, proficiency_ratings } from "../data/languages";
 
 
-const LanguageDescription = ({ index, title, icon, id, desc }) => (
+const LanguageDescription = ({ title, id, desc, proficiency }) => (
   <div>
     <a id={id} />
     <div className='mt-12 text-lg'>
-      <h2>{title}</h2>
+      <h2>{title}
+        <span className='group relative'>
+          <span className='text-lg text-tertiary-light'> &nbsp;({proficiency})</span>
+          <span className="absolute flex min-w-60 top-16  ml-8 scale-0 z-10 transition-all rounded bg-tertiary-dark p-4 text-xl font-normal text-white group-hover:scale-100">
+            {proficiency_ratings[proficiency]}
+          </span>
+        </span>
+      </h2>
+
       <div className='space-y-4'>
         {desc.map((paragraph, paragraph_index) => (
           <p key={`${id}-${paragraph_index}`} className='text-primary'>{paragraph}</p>
@@ -32,11 +40,6 @@ export default function Languages() {
 
         {languages.map((language, index) => (
           <LanguageDescription
-            title={language.title}
-            icon={language.icon}
-            id={language.id}
-            desc={language.desc}
-            index={index}
             {...language} />
         ))}
         {/* The following pads below the last entry so that following
