167e77e6f1d838f3ad93118418c87f165a256aa7
Tom Zakrajsek <tzakrajsek@gmail.com>
2024-07-14 21:09:18 -0700

    Fix type and change Portfolio -> Profile

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,8 +5,8 @@ import Navbar from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Tom Z's Portfolio",
-  description: "My portfolio, implented as a reactive website using Next.js",
+  title: "Tom Z's Profile",
+  description: "My personal profile, implemented as a reactive website using Next.js",
 };
 
 export default function RootLayout({ children }) {
