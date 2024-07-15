203762f4147e9780e41d73448279206a1e584710
Tom Zakrajsek <tzakrajsek@gmail.com>
2024-07-14 17:24:57 -0700

    Change proficiencies to use text terms

--- app/data/languages.js
+++ app/data/languages.js
@@ -11,16 +11,17 @@ import {
 } from "../assets";
 
 const proficiency_ratings = {
-  "Beginner/Novice": "Familiar with fundamental concepts, can write simple programs.",
+  "Novice": "Familiar with fundamental concepts, can write simple programs.",
+  "Functional": "Familiar with core concepts, can tackle medium tasks.  Used on-demand, but not consistently",
   "Intermediate": "Comfortable with core concepts, can tackle more complex tasks.  Familiar with idiomatic code.",
   "Advanced": "Proficient, can handle challenging problems.",
-  "Expert": "Exceptional knowledge, often consulted by peers.",
+  "Expert": "Fluent. Deep understanding of the language, including idiosyncracies.  Effective reviewer and mentor",
 }
 
 const languages = [
   {
     title: "C and C++",
-    proficiency: 10,
+    proficiency: "Expert",
     stars: "★★★★★",
     icon: c_and_cpp,
     id: "c-and-cpp",
@@ -30,13 +31,13 @@ I have decades of experience with C and C++, developing
 on Windows, OS X, and Linux using GCC, clang, and Microsoft
 Visual C++, as well numerous cross-compilers for
 embedded devices.  Much of my experience is with
-system code, as opposed to GUI applications. ★★★★★.
+system code, as opposed to GUI applications.
       `,
     ],
   },
   {
     title: "Python",
-    proficiency: 8,
+    proficiency: "Advanced",
     stars: "★★★★☆",
     icon: python,
     id: "python",
@@ -45,13 +46,12 @@ system code, as opposed to GUI applications. ★★★★★.
 I have been programming in Python for more than 10 years.
 I use it for basic scripting, text manipulation and parsing,
 data analysis, modeling, and extensive on-device test automation.
-★★★★☆.
       `,
     ]
   },
   {
     title: "Rust",
-    proficiency: 6,
+    proficiency: "Intermediate",
     stars: "★★★☆☆",
     icon: rust,
     id: "rust",
@@ -61,13 +61,12 @@ I'm ramping up on Rust right now, and I'm optimistic that it's
 a game-changer for system programmers.  The benefits of the
 memory-safety mechanisms are huge.  It also has a rich (and growing)
 standard library.  "System programmers can have nice things too."
-★★★☆☆.
       `,
     ]
   },
   {
     title: "Lua",
-    proficiency: 6,
+    proficiency: "Functional",
     stars: "★★★☆☆",
     icon: lua,
     id: "lua",
@@ -76,14 +75,13 @@ standard library.  "System programmers can have nice things too."
 I have a lot less experience with Lua than I do with Python, but I
 still think it is a great language for certain problem domains.
 Lua is extremely intuitive, compact, and easy to comprehend.
-★★★☆☆.
       `,
     ]
 
   },
   {
     title: "Mojo",
-    proficiency: 1,
+    proficiency: "Novice",
     stars: "★☆☆☆☆",
     icon: mojo,
     id: "mojo",
@@ -94,13 +92,13 @@ Lua is extremely intuitive, compact, and easy to comprehend.
   I'm just starting to work with Mojo and it looks like it has
   incredible potential.  Not only is it an extraordinary platform
   for AI, but it will probably eventually replace Python and Lua
-  in my toolbag.  ★☆☆☆☆.
+  in my toolbag.
         `,
     ]
   },
   {
     title: "Javascript",
-    proficiency: 4,
+    proficiency: "Intermediate",
     stars: "★★☆☆☆",
     icon: javascript,
     id: "javascript",
@@ -110,25 +108,25 @@ I've used Javascript (ES6) for web development, and still consider myself a novi
 But, I have been able to complete several projects and keep the code
 readable, functional, and maintainable.  It seems deceptively powerful and elegant
 if you avoid some pitfalls.  I'm exploring TypeScript, but wanted a handle
-on Javascript first.  ★★☆☆☆.
+on Javascript first.
       `,
     ]
   },
   {
     title: "Assembly",
-    proficiency: 10,
+    proficiency: "Advanced",
     stars: "★★★★★",
     icon: asm,
     id: "Assembly",
     desc: [
       `
 I've programmed in Assembly language on the following RISC, CISC, and DSP processor families:
-x86, 68k, ADSP2101, DSP56k, ARM, Snapdragon, PowerPC, MIPS, and Alpha.  ★★★★★.
+x86, 68k, ADSP2101, DSP56k, ARM, Snapdragon, PowerPC, MIPS, and Alpha.
       `,
     ]
   }, {
     title: "Other Languages",
-    proficiency: 1,
+    proficiency: "Functional",
     stars: "★☆☆☆☆",
     icon: code,
     id: "other",
@@ -137,10 +135,10 @@ x86, 68k, ADSP2101, DSP56k, ARM, Snapdragon, PowerPC, MIPS, and Alpha.  ★★�
 I've also used the following:
 C#, Ruby, Lisp, HTML, CSS, Bash, Pascal, Ada, Awk, Make, Sed, Perl, PHP, and Basic.  I'm sure
 there are others, but I don't really consider myself proficient
-in any of these.  Basically, I can make them do what I need.  ★☆☆☆☆.
+in any of these.  Basically, I can make them do what I need.
       `,
     ]
   },
 ];
 
-export { languages };
+export { languages, proficiency_ratings };
