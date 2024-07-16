bd9c8396a05cd4299c1277d01c9fde72aea25c39
Tom Zakrajsek <tzakrajsek@gmail.com>
2024-07-15 20:20:15 -0700

    Experiment with Experience display
    Factor out the code so that offering multiple views
    doesn't require so much code duplication

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -1,3 +1,4 @@
+// console.log(experiencesAtCompany.experiencesAtCompany[0]);
 import React from "react";
 
 import resolveConfig from 'tailwindcss/resolveConfig'
@@ -9,8 +10,7 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
-import { motion } from "framer-motion";
-
+import { complex, motion } from "framer-motion";
 import "react-vertical-timeline-component/style.min.css";
 
 import { experiences } from "../data/experiences";
@@ -18,32 +18,133 @@ import { AnimatedSection } from ".";
 import { textVariant } from "../utils/motion";
 import Image from "next/image";
 
-const ExperienceCard = ({ experience }) => {
+
+const TimelineElementWrapper = ({ children, company_name, date, icon, iconBg }) => {
   return (
     <VerticalTimelineElement
       // set 'visible to work around
       // https://github.com/stephane-monnot/react-vertical-timeline/issues/166
       visible={true}
       contentStyle={{
-        background: 'black', // '#123',  // '#0C1824',  // colors.secondary.dark,
+        background: colors.primary['900'],  // ref: https://tailwindcss.com/docs/customizing-colors
         color: "#fff",
         border: "1px solid #fff",
         borderRadius: "12px",
       }}
       contentArrowStyle={{ borderRight: "7px solid  #fff" }}  //#232631
-      date={experience.date}
-      iconStyle={{ background: experience.iconBg }}
+      date={date}
+      iconStyle={{ background: iconBg }}
       icon={
         <div className='flex justify-center items-center w-full h-full' >
           <Image
-            src={experience.icon}
-            alt={experience.company_name}
+            src={icon}
+            alt={company_name}
             className='w-[60%] h-[60%] object-contain'
           />
         </div >
       }
     >
+      {children}
+    </VerticalTimelineElement>
+  )
+}
 
+
+
+const LanguageTags = ({ experience }) => {
+  return (
+    <div>
+      {experience.languages != null ?
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {experience.languages.map((language, index) => (
+            <div
+              key={`lang-${index}`}
+              className={'`text-[14px] text-tertiary-light'}
+            >
+              {language}
+            </div>
+          ))}
+        </div>
+        :
+        null}
+    </div>
+  )
+}
+
+const SummaryContents = ({ experience }) => {
+  return (
+    <div>
+      <div className='my-4 ml-4 text-primary-light text-base font-normal' > {[experience.summary]} </div>
+    </div>
+  )
+}
+
+const SimpleContents = ({ experience }) => {
+  return (
+    <div>
+      <ul className='mt-5 list-disc ml-5 space-y-2'>
+        {experience.points.map((point, index) => (
+          <li
+            key={`xp-${index}`}
+            className='text-primary-light text-[14px] pl-1 tracking-wider'
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+const DetailedContents = ({ experience }) => {
+  return (
+    <div className="group perspective" >
+      <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000" >
+        <div className="relative backface-hidden group-hover:hidden " >
+          <ul className='mt-5 list-disc ml-5 space-y-2'>
+            {experience.points.map((point, index) => (
+              <li
+                key={`xp-${index}`}
+                className='text-primary-light text-[14px] pl-1 tracking-wider'
+              >
+                {point}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="relative group-not-hover my-rotate-y-180 backface-hidden overflow-hidden" >
+
+          <div className="absolute inset-0 flex items-center justify-center">
+            <p className="text-secondary-light font-bold opacity-50 z-0 -rotate-45 scale-[5] lg:scale-[8]">TL;DR</p>
+          </div>
+          {/* <h3 className='text-primary-light text-md italic mt-1' >TL;DR</h3> */}
+
+          <ul className='mt-5 list-disc ml-5 space-y-2'>
+            {experience.detailed_points.map((point, index) => (
+              <li
+                key={`dxp-${index}`}
+                className='text-primary-light text-[14px] pl-1 tracking-wider'
+              >
+                {point}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const ExperienceCard = ({ experience }) => {
+  const summary = false;
+  const company_name = experience.company_name;
+  const date = experience.date;
+  const icon = experience.icon;
+  const iconBg = experience.iconBg;
+
+  return (
+    <TimelineElementWrapper company_name={company_name} date={date} icon={icon} iconBg={iconBg}>
       <div>
         <h3 className='text-primary-light text-[24px] font-bold'>{experience.company_name}</h3>
         <h3 className='text-secondary-light text-lg font-bold mt-1' >
@@ -52,88 +153,70 @@ const ExperienceCard = ({ experience }) => {
       </div>
 
       {
-        experience.detailed_points != null ?
-
-          <div className="group perspective" >
-            <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000" >
-              <div className="relative backface-hidden group-hover:hidden " >
-                <ul className='mt-5 list-disc ml-5 space-y-2'>
-                  {experience.points.map((point, index) => (
-                    <li
-                      key={`xp-${index}`}
-                      className='text-primary-light text-[14px] pl-1 tracking-wider'
-                    >
-                      {point}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-
-              <div className="relative group-not-hover my-rotate-y-180 backface-hidden overflow-hidden" >
-
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <p className="text-secondary-light font-bold opacity-50 z-0 -rotate-45 scale-[5] lg:scale-[8]">TL;DR</p>
-                </div>
-                {/* <h3 className='text-primary-light text-md italic mt-1' >TL;DR</h3> */}
-
-                <ul className='mt-5 list-disc ml-5 space-y-2'>
-                  {experience.detailed_points.map((point, index) => (
-                    <li
-                      key={`dxp-${index}`}
-                      className='text-primary-light text-[14px] pl-1 tracking-wider'
-                    >
-                      {point}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
+        summary ?
+          <SummaryContents experience={experience} />
           :
-          <div>
-            <ul className='mt-5 list-disc ml-5 space-y-2'>
-              {experience.points.map((point, index) => (
-                <li
-                  key={`xp-${index}`}
-                  className='text-primary-light text-[14px] pl-1 tracking-wider'
-                >
-                  {point}
-                </li>
-              ))}
-            </ul>
-          </div>
+          experience.detailed_points != null ?
+            <DetailedContents experience={experience} />
+            :
+            <SimpleContents experience={experience} />
       }
 
-      <div>
-        {experience.languages != null ?
-          <div className='mt-4 flex flex-wrap gap-2'>
-            {experience.languages.map((language, index) => (
-              <div
-                key={`lang-${index}`}
-                className={'`text-[14px] text-tertiary-light'}
-              >
-                {language}
-              </div>
-            ))}
-          </div>
-          :
-          null}
-      </div>
+      {summary || <LanguageTags experience={experience} />}
 
-    </VerticalTimelineElement >
+    </TimelineElementWrapper >
+  );
+};
+
+const CompanyCard = ({ experiencesAtCompany }) => {
+  console.log(experiencesAtCompany);
+  const company_name = experiencesAtCompany[0].company_name;
+  const date = experiencesAtCompany[0].date;
+  const icon = experiencesAtCompany[0].icon;
+  const iconBg = experiencesAtCompany[0].iconBg;
+
+  return (
+    <TimelineElementWrapper company_name={company_name} date={date} icon={icon} iconBg={iconBg}>
+      <div>
+        <h3 className='text-primary-light text-lg font-bold'>{experiencesAtCompany[0].company_name}</h3>
+        <h3 className='text-secondary-light text-base font-bold mt-1' >
+          <div className='flex flex-col'>
+            {/* list-disc */}
+            {experiencesAtCompany.map((role, index) => (
+              <>
+                <div className='text-secondary-light text-base font-bold' key={`role-${index}`} > {role.title} </div>
+                {/* {
+                  role.points.map((point, pointIndex) => (
+                    <div className='text-primary-light text-base font-normal' key={`point-${pointIndex}`} > {[point]} </div>
+                  ))
+                } */}
+                <div className='my-4 ml-4 text-primary-light text-base font-normal' key={`role-${index}-summary`} > {[role.summary]} </div>
+              </>))}
+          </div>
+        </h3>
+      </div>
+    </TimelineElementWrapper>
   );
 };
 
 const Experience = () => {
+
+  const experiencesGroupedByCompany = experiences.reduce((acc, curr) => {
+    if (!acc[curr.company_name]) {
+      acc[curr.company_name] = [];
+    }
+    acc[curr.company_name].push(curr);
+    return acc;
+  }, {});
+
+  // console.log(JSON.stringify(experiencesGroupedByCompany));
+  // console.log(Object.keys(experiencesGroupedByCompany));
+
   return (
     <>
       <motion.div variants={textVariant()}>
-        <h1 className={'pre-h1 text-center'}>
-          Career
-        </h1>
-        <h1 className={'text-center'}>
-          Work Experience
-        </h1>
+        <h1 className={'pre-h1 text-center'}>Career</h1>
+        <h1 className={'text-center'}>Work Experience</h1>
       </motion.div>
 
       <div className='mt-8 lg:mt-16 flex flex-col'>
@@ -144,6 +227,13 @@ const Experience = () => {
               experience={experience}
             />
           ))}
+
+          {/* {Object.keys(experiencesGroupedByCompany).map((company, index) => (
+            // console.log(company),
+            // console.log(JSON.stringify(experiencesGroupedByCompany[company])),
+            <CompanyCard experiencesAtCompany={experiencesGroupedByCompany[company]} />
+          ))} */}
+
         </VerticalTimeline>
       </div>
     </>
