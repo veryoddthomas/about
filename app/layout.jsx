e9aa31dfe32e6daaaf801b9665112f09c9a02a90
Tom Zakrajsek <tzakrajsek@gmail.com>
2024-07-17 23:40:01 -0700

    Set default color and font size

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`} >
+      <body className={`${inter.className} antialiased text-primary text-xl`} >
         <Navbar />
         {children}
       </body>
