835d7a9dd7a005042680b0e516e080e4f268977a
Tom Zakrajsek <tzakrajsek@gmail.com>
2024-08-22 23:15:31 -0700

    Switch back to CompanyCard

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -216,18 +216,18 @@ const Experience = () => {
 
       <div className='mt-8 lg:mt-16 flex flex-col'>
         <VerticalTimeline>
-          {experiences.map((experience, index) => (
+          {/* {experiences.map((experience, index) => (
             <ExperienceCard
               key={`experience-${index}`}
               experience={experience}
             />
-          ))}
+          ))} */}
 
-          {/* {Object.keys(experiencesGroupedByCompany).map((company, index) => (
+          {Object.keys(experiencesGroupedByCompany).map((company, index) => (
             // console.log(company),
             // console.log(JSON.stringify(experiencesGroupedByCompany[company])),
             <CompanyCard experiencesAtCompany={experiencesGroupedByCompany[company]} />
-          ))} */}
+          ))}
 
         </VerticalTimeline>
       </div>
