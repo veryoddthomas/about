1cec299643dfc22572f29dcdc76ba4cb71e4455d
Tom Zakrajsek <tzakrajsek@gmail.com>
2024-08-23 15:08:35 -0700

    Remove unwanted padding on Contact Me section
    This caused a weird left indent that was not wanted.
    
    Recommend diff -w for review.  Whitespace changed a lot.

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -69,94 +69,89 @@ const Contact = () => {
   };
 
   return (
-    <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    <motion.div
+      variants={slideIn("left", "tween", 0.2, 1)}
+      className='flex-[0.75] bg-black-100 rounded-2xl'
     >
-      <motion.div
-        variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      <h1 className='pre-h1'>Get in touch</h1>
+      <h1>Contact Me</h1>
+
+      <div className='font-medium text-primary lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-6'>
+        {/* <div className="flex gap-6 mt-8"> */}
+        {/* <span>Visit my public platforms <FontAwesomeIcon icon={faLinkedin} /> <FontAwesomeIcon icon={faGithub} /></span> */}
+        <span>Visit my public platforms</span>
+        &nbsp;
+        &nbsp;
+        <Link rel="noopener noreferrer" target="_blank" href={'https://github.com/veryoddthomas/'}>
+          <FontAwesomeIcon icon={faGithub} inverse />
+        </Link>
+        &nbsp;
+        &nbsp;
+        <Link rel="noopener noreferrer" target="_blank" href={'https://www.linkedin.com/in/tom-zakrajsek/'}>
+          <FontAwesomeIcon icon={faLinkedin} inverse />
+        </Link>
+        &nbsp;
+        &nbsp;
+        <span>or just drop me a line below.</span>&nbsp;&nbsp;<span className='text-tertiary'>Feedback about the site is welcome.</span>
+        {/* </div> */}
+      </div>
+
+
+      <form
+        ref={formRef}
+        onSubmit={handleSubmit}
+        className='mt-12 flex flex-col gap-8'
       >
-        <h1 className='pre-h1'>Get in touch</h1>
-        <h1>Contact Me</h1>
 
-        <div className='font-medium text-primary lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-6'>
-          {/* <div className="flex gap-6 mt-8"> */}
-          {/* <span>Visit my public platforms <FontAwesomeIcon icon={faLinkedin} /> <FontAwesomeIcon icon={faGithub} /></span> */}
-          <span>Visit my public platforms</span>
-          &nbsp;
-          &nbsp;
-          <Link rel="noopener noreferrer" target="_blank" href={'https://github.com/veryoddthomas/'}>
-            <FontAwesomeIcon icon={faGithub} inverse />
-          </Link>
-          &nbsp;
-          &nbsp;
-          <Link rel="noopener noreferrer" target="_blank" href={'https://www.linkedin.com/in/tom-zakrajsek/'}>
-            <FontAwesomeIcon icon={faLinkedin} inverse />
-          </Link>
-          &nbsp;
-          &nbsp;
-          <span>or just drop me a line below.</span>&nbsp;&nbsp;<span className='text-tertiary'>Feedback about the site is welcome.</span>
-          {/* </div> */}
-        </div>
-
-
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8'
-        >
-
-          {/* <button
+        {/* <button
             type='submit'
             className='bg-white py-3 px-8 rounded-xl outline-none w-fit text-tertiary font-bold shadow-md shadow-primary'
           >
             {loading ? "Sending..." : "Send"}
           </button> */}
 
-          <label className='flex flex-col'>
-            {/* <span className='text-white font-medium mb-4'>Your Name</span> */}
-            <input
-              type='text'
-              name='name'
-              value={form.name}
-              onChange={handleChange}
-              placeholder="Your name here"
-              className='bg-primary-dark py-4 px-6 placeholder:text-primary-default text-white rounded-lg outline-none border-2 font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            {/* <span className='text-white font-medium mb-4'>Your email</span> */}
-            <input
-              type='email'
-              name='email'
-              value={form.email}
-              onChange={handleChange}
-              placeholder="Your email address here"
-              className='bg-primary-dark py-4 px-6 placeholder:text-primary-default text-white rounded-lg outline-none border-2 font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            {/* <span className='text-white font-medium mb-4'>Your Message</span> */}
-            <textarea
-              rows={7}
-              name='message'
-              value={form.message}
-              onChange={handleChange}
-              placeholder='Message...'
-              className='bg-primary-dark py-4 px-6 placeholder:text-primary-default text-primary-light rounded-lg outline-none border-2 font-medium'
-            />
-          </label>
+        <label className='flex flex-col'>
+          {/* <span className='text-white font-medium mb-4'>Your Name</span> */}
+          <input
+            type='text'
+            name='name'
+            value={form.name}
+            onChange={handleChange}
+            placeholder="Your name here"
+            className='bg-primary-dark py-4 px-6 placeholder:text-primary-default text-white rounded-lg outline-none border-2 font-medium'
+          />
+        </label>
+        <label className='flex flex-col'>
+          {/* <span className='text-white font-medium mb-4'>Your email</span> */}
+          <input
+            type='email'
+            name='email'
+            value={form.email}
+            onChange={handleChange}
+            placeholder="Your email address here"
+            className='bg-primary-dark py-4 px-6 placeholder:text-primary-default text-white rounded-lg outline-none border-2 font-medium'
+          />
+        </label>
+        <label className='flex flex-col'>
+          {/* <span className='text-white font-medium mb-4'>Your Message</span> */}
+          <textarea
+            rows={7}
+            name='message'
+            value={form.message}
+            onChange={handleChange}
+            placeholder='Message...'
+            className='bg-primary-dark py-4 px-6 placeholder:text-primary-default text-primary-light rounded-lg outline-none border-2 font-medium'
+          />
+        </label>
 
-          <button
-            type='submit'
-            className=' bg-primary-dark py-3 px-8 rounded-xl w-fit text-secondary-light font-bold shadow-md border-2 shadow-primary'
-          >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
-      </motion.div>
-
-    </div>
+        <button
+          type='submit'
+          className=' bg-primary-dark py-3 px-8 rounded-xl w-fit text-secondary-light font-bold shadow-md border-2 shadow-primary'
+        >
+          {loading ? "Sending..." : "Send"}
+        </button>
+      </form>
+    </motion.div>
   );
 };
 
